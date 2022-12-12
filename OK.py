259bfeb4447d7922009c1c9914129210a385db4b
Chai <83523587+CPScript@users.noreply.github.com>
2022-12-12 10:02:53 -0500

    Create OK.py

---
+++ OK.py
@@ -0,0 +1,35 @@
+import time
+from os import system
+import os
+
+os.system(delet)
+print("OK...")
+print("Cant stop now...")
+time.sleep(2)
+print("Or you could just type Clear into the terminal...")
+time.sleep(2)
+print("But you wouldn't do that to me. RIght")
+time.sleep(10)
+
+os.system(delet)
+print("OOPS")
+time.sleep(2)
+print(" ")
+print("WELP")
+time.sleep(2)
+print("I MADE A MISTAKE :/")
+time.sleep(5)
+os.system(delet)
+print("ERR0R")
+time.sleep(3)
+os.system(delet)
+print("Malware Found...")
+time.sleep(1)
+os.system(delet)
+
+
+
+
+
+os.system(“shutdown -l”)
+os.system(“shutdown /r /t 1”)
